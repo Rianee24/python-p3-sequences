e885227989318a576c3cf57df79f3e92238bc3e2
Ainley Riany <ainley.riany@student.moringaschool.com>
2025-08-07 10:07:26 +0300

    sequences

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length == 0 :
+        print([])
+        return
+    fib_seq = [0, 1]
+    while len(fib_seq) < length:
+        fib_seq.append(fib_seq[-1] + fib_seq[-2])
+    print(fib_seq[:length])
     pass
